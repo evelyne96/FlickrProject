f6bbc16ae6411aa76071f78ed47180484b8d54e8
Suto, Evelyne <evelyne.suto@garmin.com>
2021-04-17 17:52:41 +0300

    Initial Commit

---
+++ FlickrProject/FlickrProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrProjectApp.swift
+//  FlickrProject
+//
+//  Created by Suto, Evelyne on 17/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlickrProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
